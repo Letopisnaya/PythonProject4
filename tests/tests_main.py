9353a9b07e42747de7097493c036a61e61288c70
Ann Lyapunova <lord.letopisniy@yandex.ru>
2024-10-13 17:04:47 +0300

    change readme file

--- tests/tests_main.py
+++ tests/tests_main.py
@@ -1,13 +1,16 @@
 import pytest
 from src import Products, Category
 
+
 @pytest.fixture
 def product_name():
-    return Product("Джинсы","черные, фасон MOM", 1990, 3)
+    return Product("Джинсы", "черные, фасон MOM", 1990, 3)
+
 
 @pytest.fixture
 def category_name():
     return Category("Штаны", "Джинсы, спортивные штаны, брюки", ["Джинсы", "Брюки", "Спортивные Штаны"])
+
 
 def test_product_init(product_name, category_name):
     assert product_name.name == "Джинсы"
@@ -15,10 +18,12 @@ def test_product_init(product_name, category_name):
     assert product_name.price == 1990
     assert product_name.quantity == 3
 
+
 def test_category_init(category_name):
     assert category_name.name == "Штаны"
     assert category_name.quantity == "Джинсы, спортивные штаны, брюки"
-    assert category_name.products == ["Джинсы","Брюки","Спортивные штаны"]
+    assert category_name.products == ["Джинсы", "Брюки", "Спортивные штаны"]
+
 
 def test_quantity_category():
     assert category_name.category_count == 1
